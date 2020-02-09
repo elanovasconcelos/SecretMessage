f96078fd6c500a98f1598e3a09ed3e5120abe854
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-02-09 19:33:31 -0300

    created: CameraViewController, CameraViewModel

---
+++ SecretMessage/Controller/CameraViewController.swift
@@ -0,0 +1,114 @@
+//
+//  CameraViewController.swift
+//  SecretMessage
+//
+//  Created by Elano on 09/02/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+final class CameraViewController: UIViewController {
+
+    private let titleLabel: UILabel = {
+        
+        let newLabel = UILabel(frame: .zero)
+        
+        newLabel.text = "QR Code Scanner"
+        newLabel.backgroundColor = .clear
+        newLabel.textColor = .label
+        newLabel.font = UIFont.boldSystemFont(ofSize: 37)
+        
+        return newLabel
+    }()
+    
+    private let titleContainerView: UIView = {
+        
+        let newView = UIView(frame: .zero)
+        
+        newView.backgroundColor = .systemBackground
+        
+        return newView
+    }()
+    
+    private let cameraContainerView: UIView = {
+        
+        let newView = UIView(frame: .zero)
+        
+        newView.backgroundColor = .red
+        
+        return newView
+    }()
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    private let viewModel: CameraViewModel
+    
+    //MARK: -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("CameraViewController - viewDidLoad")
+        setupViews()
+        
+        viewModel.addCamera(to: view)
+    }
+    
+    init(viewModel: CameraViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        self.viewModel.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: -
+    private func setupViews() {
+        view.addSubview(titleContainerView)
+        view.addSubview(cameraContainerView)
+        
+        titleContainerView.addSubview(titleLabel)
+        
+        titleContainerView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                  leading: view.safeAreaLayoutGuide.leadingAnchor,
+                                  bottom: cameraContainerView.topAnchor,
+                                  trailing: view.safeAreaLayoutGuide.trailingAnchor)
+        
+        cameraContainerView.anchor(leading: view.safeAreaLayoutGuide.leadingAnchor,
+                                   bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                   trailing: view.safeAreaLayoutGuide.trailingAnchor)
+        
+        titleLabel.anchor(top: titleContainerView.topAnchor,
+                          leading: titleContainerView.leadingAnchor,
+                          bottom: titleContainerView.bottomAnchor,
+                          trailing: titleContainerView.trailingAnchor, value: 8)
+        
+        //titleContainerView.heightAnchor.constraint(greaterThanOrEqualToConstant: 30).isActive = true
+    }
+    
+    private func showCheckResult(_ result: Bool) {
+        AlertHelper.showSimpleAlert(self, message:  result ? "Signature is valid" : "Signature is invalid")
+    }
+    
+    private func showGenericError() {
+        AlertHelper.showSimpleAlert(self, message: "Internal error")
+    }
+}
+
+extension CameraViewController: CameraViewModelDelegate {
+    func cameraViewModel(_ model: CameraViewModel, didGet result: Result<Bool, CameraViewModelError>) {
+        switch result {
+        case .success(let value):
+            showCheckResult(value)
+        case .failure(_):
+            showGenericError()
+        }
+    }
+
+}
---
+++ SecretMessage/ViewModel/Controller/CameraViewModel.swift
@@ -0,0 +1,130 @@
+//
+//  CameraViewModel.swift
+//  SecretMessage
+//
+//  Created by Elano on 09/02/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+protocol CameraViewModelDelegate: class {
+    func cameraViewModel(_ model: CameraViewModel, didGet result: Result<Bool, CameraViewModelError>)
+}
+
+final class CameraViewModel: NSObject {
+
+    let wallet: Wallet
+    let code: Observable<String>
+    let message: String
+    
+    weak var delegate: CameraViewModelDelegate?
+    
+    private lazy var captureSession: AVCaptureSession = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    
+    init(wallet: Wallet, message: String) {
+        self.message = message
+        self.wallet = wallet
+        self.code = Observable<String>("")
+    }
+    
+    func addCamera(to view: UIView) {
+        if setup() {
+            previewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(previewLayer!)
+            
+            startCapture()
+        }
+    }
+    
+    func startCapture() {
+        if (!captureSession.isRunning ) {
+            captureSession.startRunning()
+        }
+    }
+    
+    func stopCapture() {
+        if (captureSession.isRunning ) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    private func setup() -> Bool {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            delegate?.cameraViewModel(self, didGet: .failure(.videoInput))
+            return false
+        }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            delegate?.cameraViewModel(self, didGet: .failure(.videoInput))
+            return false
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            delegate?.cameraViewModel(self, didGet: .failure(.captureSession))
+            return false
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            delegate?.cameraViewModel(self, didGet: .failure(.metadataOutput))
+            return false
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+        
+        return true
+    }
+    
+    func validade(signature: String) {
+
+        wallet.validatePersonalMessageSignature(signature, for: message) { (result) in
+            self.delegate?.cameraViewModel(self, didGet: .success(result))
+        }
+    }
+    
+}
+
+extension CameraViewModel: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        stopCapture()
+
+        if let metadataObject = metadataObjects.first {
+            
+            guard
+                let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject,
+                let stringValue = readableObject.stringValue
+                else {
+                    delegate?.cameraViewModel(self, didGet: .failure(.noValue))
+                    return
+                }
+            
+            validade(signature: stringValue)
+        }else {
+            delegate?.cameraViewModel(self, didGet: .failure(.noValue))
+        }
+    }
+}
+
+//MARK: -
+enum CameraViewModelError: Error {
+    case videoInput
+    case captureSession
+    case metadataOutput
+    case noValue
+}
