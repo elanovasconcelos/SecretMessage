d4fb65a89f106574167c2533d1957a9bc87508de
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-02-11 21:44:18 -0300

    [LoginViewModelTests] fixing tests

--- SecretMessageTests/ViewModel/Controller/LoginViewModelTests.swift
+++ SecretMessageTests/ViewModel/Controller/LoginViewModelTests.swift
@@ -21,20 +21,24 @@ class LoginViewModelTests: XCTestCase {
         
     }
 
-    func testModels() {
+    func testModelsField() {
         let models = loginViewModel.models
         
         XCTAssertEqual(models.count, 2)
-        XCTAssertEqual(models[0].title, "Setup")
-        XCTAssertEqual(models[1].title, "Private key")
+        
+        if models.count >= 2 {
+            XCTAssertEqual(models[0].title, "Setup")
+            XCTAssertEqual(models[1].title, "Private key")
+        }
     }
 
     func testFailLogin() {
+        let textFieldText = "no key"
         let expectation = self.expectation(description: "Delegate")
         let mockDelegate = MockLoginViewModelDelegate() { [loginViewModel] delegate in
             
             XCTAssertEqual(delegate.callCount, 1)
-            XCTAssertEqual(loginViewModel?.textFieldCellViewModel.text.value, "no key")
+            XCTAssertEqual(loginViewModel?.textFieldCellViewModel.text.value, textFieldText)
             XCTAssertNotNil(delegate.result)
             
             switch delegate.result! {
@@ -46,12 +50,14 @@ class LoginViewModelTests: XCTestCase {
         }
         
         loginViewModel.delegate = mockDelegate
-        loginViewModel.textFieldCellViewModel.text.value = "no key"
-
+        loginViewModel.textFieldCellViewModel.text.value = textFieldText
+        loginViewModel.textFieldCellViewModel(loginViewModel.textFieldCellViewModel, didKeyboardReturnsWith: textFieldText)
+        
         waitForExpectations(timeout: 1, handler: nil)
     }
     
     func testSucessLogin() {
+        let textFieldText = "42c521d5630bedc9a408725ad7f11690999c125a1fcebe248f24b6b6d8766c17"
         let expectation = self.expectation(description: "Delegate")
         let mockDelegate = MockLoginViewModelDelegate() { delegate in
             
@@ -69,7 +75,8 @@ class LoginViewModelTests: XCTestCase {
         }
         
         loginViewModel.delegate = mockDelegate
-        loginViewModel.textFieldCellViewModel.text.value = "42c521d5630bedc9a408725ad7f11690999c125a1fcebe248f24b6b6d8766c17"
+        loginViewModel.textFieldCellViewModel.text.value = textFieldText
+        loginViewModel.textFieldCellViewModel(loginViewModel.textFieldCellViewModel, didKeyboardReturnsWith: textFieldText)
 
         waitForExpectations(timeout: 1, handler: nil)
     }
